f58611d0336dd0a6babe02a41cf4b4990c6ad2ac
Bernd Rücker <bernd.ruecker@camunda.com>
2020-05-07 19:53:42 +0200

    added test infrastructure

---
+++ src/test/java/io/zeebe/lambda/NoTestsYet.java
@@ -0,0 +1,16 @@
+package io.zeebe.lambda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoTestsYet {
+  
+  @Test
+  public void noOpTest() throws Exception {
+    System.out.println("##########");
+  }
+}
